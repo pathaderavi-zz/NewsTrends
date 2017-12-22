b6829dbadb5d1132bf14b9ffb96fc4ecfb766d91
Ravikiran Pathade <pathaderavi@gmail.com>
2017-12-22 04:24:16 -0500

    Notifications Created

--- app/src/main/java/services/JobDispatcherForNotifications.java
+++ app/src/main/java/services/JobDispatcherForNotifications.java
@@ -1,8 +1,16 @@
 package services;
 
 import android.annotation.SuppressLint;
+
+import com.example.ravikiranpathade.newstrends.R;
+import com.example.ravikiranpathade.newstrends.activities.MainActivity;
 import com.firebase.jobdispatcher.JobParameters;
 import com.firebase.jobdispatcher.JobService;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 /**
@@ -11,16 +19,34 @@ import android.widget.Toast;
 
 @SuppressLint("NewApi")
 public class JobDispatcherForNotifications extends JobService {
+    NotificationCompat.Builder notification;
+    private static final int NOTIFICATION_ID = 80878085;
+    private static final String CHANNEL_ID = "NEWSTRENDS";
+
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
-        Toast.makeText(getApplicationContext(),"Service is running",Toast.LENGTH_SHORT).show();
-        jobFinished(jobParameters,false);
+        notification = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID);
+        notification.setAutoCancel(true);
+        notification.setSmallIcon(R.drawable.ic_favorite_black_24px)
+                .setTicker("Testing Notifications")
+                .setWhen(System.currentTimeMillis())
+                .setContentTitle("You have new NEWS Alerts")
+                .setContentText("Open The App");
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),0,intent,PendingIntent.FLAG_CANCEL_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(NOTIFICATION_ID,notification.build());
+
+        jobFinished(jobParameters, false);
         return true;
     }
 
     @Override
     public boolean onStopJob(JobParameters jobParameters) {
-      
+
         return false;
     }
 }
